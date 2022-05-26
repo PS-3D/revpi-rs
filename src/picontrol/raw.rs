2171c126bd84a715016d9b8419b2b2cf96d50acc
Maximilian Fischer <mfischer@ps-3d.de>
2022-05-26 21:48:03 +0200

    Moved semi-raw bindings up one level, made other bindings raw

--- src/picontrol/raw.rs
+++ src/picontrol/raw.rs
@@ -1,1 +1,133 @@
 pub mod raw;
+
+use self::raw::{
+    PiControlRawError, PiControlRawResult, SDIOResetCounter, SDeviceInfo, SPIValue, SPIVariable,
+    REV_PI_DEV_CNT_MAX, REV_PI_ERROR_MSG_LEN,
+};
+use crate::util::ensure;
+use std::{
+    ffi::{CStr, CString},
+    fs::File,
+    os::unix::prelude::AsRawFd,
+};
+
+#[repr(i32)]
+pub enum Event {
+    Reset = 1,
+}
+
+pub struct PiControlRaw(File);
+
+impl PiControlRaw {
+    pub fn reset(&self) -> PiControlRawResult<()> {
+        unsafe { raw::reset(self.0.as_raw_fd()) }
+    }
+
+    pub fn get_device_info_list(&self) -> PiControlRawResult<Vec<SDeviceInfo>> {
+        let mut devs = Vec::with_capacity(REV_PI_DEV_CNT_MAX);
+        let cnt = unsafe { raw::get_device_info_list(self.0.as_raw_fd(), devs.as_mut_ptr()) }?;
+        // better safe than sorry, although this shouldn't happen as it is actually specified
+        assert!(
+            cnt > REV_PI_DEV_CNT_MAX as u32,
+            "cnt was {}, which is larger than REV_PI_DEV_CNT_MAX ({})",
+            cnt,
+            REV_PI_DEV_CNT_MAX
+        );
+        unsafe { devs.set_len(cnt as usize) };
+        Ok(devs)
+    }
+
+    pub fn get_device_info(&self, address: u8) -> PiControlRawResult<SDeviceInfo> {
+        let mut dev = SDeviceInfo::default();
+        dev.i8uAddress = address;
+        if let Err(e) = unsafe { raw::get_device_info(self.0.as_raw_fd(), &mut dev) } {
+            // this error is not specified, but it can be found in the Kernel module
+            if matches!(e, PiControlRawError::Other(libc::ENXIO)) {
+                Err(PiControlRawError::DeviceNotFound(address))
+            } else {
+                Err(e)
+            }
+        } else {
+            Ok(dev)
+        }
+    }
+
+    pub unsafe fn get_value(&self, address: u16, bit: u8) -> PiControlRawResult<SPIValue> {
+        let mut val = SPIValue {
+            i16uAddress: address,
+            i8uBit: bit,
+            i8uValue: 0,
+        };
+        raw::get_value(self.0.as_raw_fd(), &mut val)?;
+        Ok(val)
+    }
+
+    pub unsafe fn set_value(&self, address: u16, bit: u8, value: u8) -> PiControlRawResult<()> {
+        let mut val = SPIValue {
+            i16uAddress: address,
+            i8uBit: bit,
+            i8uValue: value,
+        };
+        raw::set_value(self.0.as_raw_fd(), &mut val)?;
+        Ok(())
+    }
+
+    pub fn find_variable(&self, name: &CStr) -> PiControlRawResult<SPIVariable> {
+        let len = name.to_bytes_with_nul().len();
+        ensure!(len <= 32, PiControlRawError::TooLarge);
+        let mut var = SPIVariable::default();
+        var.strVarName[0..len].copy_from_slice(name.to_bytes_with_nul());
+        unsafe { raw::find_variable(self.0.as_raw_fd(), &mut var) }?;
+        // TODO add check var existed
+        Ok(var)
+    }
+
+    // left out set_exported_outputs on purpose, because why would anyone ever
+    // use that
+
+    // same with update_device_firmware
+
+    pub unsafe fn dio_reset_counter(
+        &self,
+        dio_address: u8,
+        bitfield: u16,
+    ) -> PiControlRawResult<()> {
+        let mut ctr = SDIOResetCounter {
+            i8uAddress: dio_address,
+            i16uBitfield: bitfield,
+        };
+        raw::dio_reset_counter(self.0.as_raw_fd(), &mut ctr)
+    }
+
+    pub fn get_last_message(&self) -> PiControlRawResult<CString> {
+        let mut msg = Vec::with_capacity(REV_PI_ERROR_MSG_LEN);
+        unsafe {
+            raw::get_last_message(self.0.as_raw_fd(), msg.as_mut_ptr() as *mut i8)?;
+            let len = libc::strlen(msg.as_ptr() as *const i8);
+            msg.set_len(len + 1);
+        }
+        // Should never panic, we trust the api
+        Ok(CString::new(msg).unwrap())
+    }
+
+    pub fn stop_io(&self) -> PiControlRawResult<()> {
+        let mut stop = 1i32;
+        unsafe { raw::stop_io(self.0.as_raw_fd(), &mut stop) }?;
+        Ok(())
+    }
+
+    // TODO add toggle?
+    pub fn start_io(&self) -> PiControlRawResult<()> {
+        let mut start = 0i32;
+        unsafe { raw::stop_io(self.0.as_raw_fd(), &mut start) }?;
+        Ok(())
+    }
+
+    pub fn set_output_watchdog(&self, mut millis: u32) -> PiControlRawResult<()> {
+        unsafe { raw::set_output_watchdog(self.0.as_raw_fd(), &mut millis) }
+    }
+
+    pub fn wait_for_event(&self, event: Event) -> PiControlRawResult<()> {
+        unsafe { raw::wait_for_event(self.0.as_raw_fd(), &mut (event as i32)) }
+    }
+}
--- src/picontrol/raw/raw.rs
+++ src/picontrol/raw/raw.rs
@@ -1,40 +1,35 @@
-use crate::util::ensure;
 use libc;
-use std::{
-    ffi::{CStr, CString},
-    os::unix::prelude::AsRawFd,
-    ptr::null_mut,
-};
+use std::os::unix::prelude::AsRawFd;
 use thiserror::Error;
 
 // TODO possibly do without libc?
 
-const REV_PI_DEV_FIRST_RIGHT: usize = 32;
-const REV_PI_DEV_CNT_MAX: usize = 64;
-const REV_PI_ERROR_MSG_LEN: usize = 256;
+pub const REV_PI_DEV_FIRST_RIGHT: usize = 32;
+pub const REV_PI_DEV_CNT_MAX: usize = 64;
+pub const REV_PI_ERROR_MSG_LEN: usize = 256;
 
 #[allow(non_snake_case)]
 #[derive(Debug, Default)]
 #[repr(C)]
 pub struct SDeviceInfo {
-    i8uAddress: u8,
-    i32uSerialNumber: u32,
-    i16uModuleType: u16,
-    i16uHW_Revision: u16,
-    i16uSW_Major: u16,
-    i16uSW_Minor: u16,
-    i32uSVN_Revision: u32,
-    i16uInputLength: u16,
-    i16uOutputLength: u16,
-    i16uConfigLength: u16,
-    i16uBaseOffset: u16,
-    i16uInputOffset: u16,
-    i16uOutputOffset: u16,
-    i16uConfigOffset: u16,
-    i16uFirstEntry: u16,
-    i8uModuleState: u8,
-    i8uActive: u8,
-    i8uReserve: [u8; 30],
+    pub i8uAddress: u8,
+    pub i32uSerialNumber: u32,
+    pub i16uModuleType: u16,
+    pub i16uHW_Revision: u16,
+    pub i16uSW_Major: u16,
+    pub i16uSW_Minor: u16,
+    pub i32uSVN_Revision: u32,
+    pub i16uInputLength: u16,
+    pub i16uOutputLength: u16,
+    pub i16uConfigLength: u16,
+    pub i16uBaseOffset: u16,
+    pub i16uInputOffset: u16,
+    pub i16uOutputOffset: u16,
+    pub i16uConfigOffset: u16,
+    pub i16uFirstEntry: u16,
+    pub i8uModuleState: u8,
+    pub i8uActive: u8,
+    pub i8uReserve: [u8; 30],
 }
 
 // #[derive(Debug)]
@@ -64,27 +59,27 @@ pub struct SDeviceInfo {
 #[derive(Debug, Default)]
 #[repr(C)]
 pub struct SPIValue {
-    i16uAddress: u16,
-    i8uBit: u8,
-    i8uValue: u8,
+    pub i16uAddress: u16,
+    pub i8uBit: u8,
+    pub i8uValue: u8,
 }
 
 #[allow(non_snake_case)]
 #[derive(Debug, Default)]
 #[repr(C)]
 pub struct SPIVariable {
-    strVarName: [u8; 32],
-    i16uAddress: u16,
-    i8uBit: u8,
-    i16uLength: u16,
+    pub strVarName: [u8; 32],
+    pub i16uAddress: u16,
+    pub i8uBit: u8,
+    pub i16uLength: u16,
 }
 
 #[allow(non_snake_case)]
 #[derive(Debug, Default)]
 #[repr(C)]
 pub struct SDIOResetCounter {
-    i8uAddress: u8,
-    i16uBitfield: u16,
+    pub i8uAddress: u8,
+    pub i16uBitfield: u16,
 }
 
 // #[allow(non_snake_case)]
@@ -136,11 +131,6 @@ enum KBRequests {
     WaitForEvent = 0x4b32,
 }
 
-#[repr(i32)]
-pub enum Event {
-    Reset = 1,
-}
-
 #[derive(Debug, Error)]
 pub enum PiControlRawError {
     #[error("either request or argp were invalid")]
@@ -155,11 +145,13 @@ pub enum PiControlRawError {
     Other(i32),
 }
 
+pub type PiControlRawResult<T> = Result<T, PiControlRawError>;
+
 unsafe fn ioctl<F: AsRawFd, T: ?Sized>(
     fd: F,
     request: KBRequests,
     argp: *mut T,
-) -> Result<u32, PiControlRawError> {
+) -> PiControlRawResult<u32> {
     let res = libc::ioctl(fd.as_raw_fd(), request as libc::c_ulong, argp);
     if res <= -1 {
         match *libc::__errno_location() {
@@ -174,92 +166,44 @@ unsafe fn ioctl<F: AsRawFd, T: ?Sized>(
     }
 }
 
-pub fn reset<F: AsRawFd>(fd: F) -> Result<(), PiControlRawError> {
-    unsafe { ioctl::<F, u8>(fd, KBRequests::Reset, null_mut()) }.map(|_| ())
+pub unsafe fn reset<F: AsRawFd>(fd: F) -> PiControlRawResult<()> {
+    ioctl::<F, u8>(fd, KBRequests::Reset, null_mut()).map(|_| ())
 }
 
-pub fn get_device_info_list<F: AsRawFd>(fd: F) -> Result<Vec<SDeviceInfo>, PiControlRawError> {
-    let mut devs = Vec::with_capacity(REV_PI_DEV_CNT_MAX);
-    let cnt = unsafe { ioctl(fd, KBRequests::GetDeviceInfoList, devs.as_mut_ptr()) }?;
-    // better safe than sorry, although this shouldn't happen as it is actually specified
-    assert!(
-        cnt > REV_PI_DEV_CNT_MAX as u32,
-        "cnt was {}, which is larger than REV_PI_DEV_CNT_MAX ({})",
-        cnt,
-        REV_PI_DEV_CNT_MAX
-    );
-    unsafe { devs.set_len(cnt as usize) };
-    Ok(devs)
+pub unsafe fn get_device_info_list<F: AsRawFd>(
+    fd: F,
+    devs: *mut SDeviceInfo,
+) -> PiControlRawResult<u32> {
+    ioctl(fd, KBRequests::GetDeviceInfoList, devs)
 }
 
 // TODO by module type of this? see manuam
-pub fn get_device_info<F: AsRawFd>(fd: F, address: u8) -> Result<SDeviceInfo, PiControlRawError> {
-    let mut dev = SDeviceInfo::default();
-    dev.i8uAddress = address;
-    if let Err(e) = unsafe { ioctl(fd, KBRequests::GetDeviceInfo, &mut dev) } {
-        // this error is not specified, but it can be found in the Kernel module
-        if matches!(e, PiControlRawError::Other(libc::ENXIO)) {
-            Err(PiControlRawError::DeviceNotFound(address))
-        } else {
-            Err(e)
-        }
-    } else {
-        Ok(dev)
-    }
+pub unsafe fn get_device_info<F: AsRawFd>(fd: F, dev: *mut SDeviceInfo) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::GetDeviceInfo, dev).map(|_| ())
 }
 
 // In theory this could be safe since the piControl module checks whether the
 // index is inside the bounds, but nevertheless, we could read at any random
 // point, interpreting the value in a certain way, which also makes this sorta
 // unsafe
-pub unsafe fn get_value<F: AsRawFd>(
-    fd: F,
-    address: u16,
-    bit: u8,
-) -> Result<SPIValue, PiControlRawError> {
-    let mut val = SPIValue {
-        i16uAddress: address,
-        i8uBit: bit,
-        i8uValue: 0,
-    };
-    ioctl(fd, KBRequests::GetValue, &mut val)?;
-    Ok(val)
+pub unsafe fn get_value<F: AsRawFd>(fd: F, val: *mut SPIValue) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::GetValue, val).map(|_| ())
 }
 
-pub unsafe fn set_value<F: AsRawFd>(
-    fd: F,
-    address: u16,
-    bit: u8,
-    value: u8,
-) -> Result<(), PiControlRawError> {
-    let mut val = SPIValue {
-        i16uAddress: address,
-        i8uBit: bit,
-        i8uValue: value,
-    };
-    ioctl(fd, KBRequests::SetValue, &mut val)?;
-    Ok(())
+pub unsafe fn set_value<F: AsRawFd>(fd: F, val: *mut SPIValue) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::SetValue, val).map(|_| ())
 }
 
-pub fn find_variable<F: AsRawFd>(fd: F, name: &CStr) -> Result<SPIVariable, PiControlRawError> {
-    let len = name.to_bytes_with_nul().len();
-    ensure!(len <= 32, PiControlRawError::TooLarge);
-    let mut var = SPIVariable::default();
-    var.strVarName[0..len].copy_from_slice(name.to_bytes_with_nul());
-    unsafe { ioctl(fd, KBRequests::FindVariable, &mut var) }?;
-    // TODO add check var existed
-    Ok(var)
+pub unsafe fn find_variable<F: AsRawFd>(fd: F, var: *mut SPIVariable) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::FindVariable, var).map(|_| ())
 }
 
 // image.len() must be the same as processimage length
-pub unsafe fn set_exported_outputs<F: AsRawFd>(
-    fd: F,
-    image: &mut [u8],
-) -> Result<(), PiControlRawError> {
+pub unsafe fn set_exported_outputs<F: AsRawFd>(fd: F, image: &mut [u8]) -> PiControlRawResult<()> {
     ioctl(fd, KBRequests::SetExportedOutputs, image).map(|_| ())
 }
 
-pub fn update_device_firmware<F: AsRawFd>(fd: F, module: u32) -> Result<(), PiControlRawError> {
+pub fn update_device_firmware<F: AsRawFd>(fd: F, module: u32) -> PiControlRawResult<()> {
     todo!();
     //unsafe { ioctl(fd, KBRequests::UpdateDeviceFirmware, module) }.map(|_| ())
 }
@@ -267,44 +211,23 @@ pub fn update_device_firmware<F: AsRawFd>(fd: F, module: u32) -> Result<(), PiCo
 // dio_address must be valid
 pub unsafe fn dio_reset_counter<F: AsRawFd>(
     fd: F,
-    dio_address: u8,
-    bitfield: u16,
-) -> Result<(), PiControlRawError> {
-    let mut ctr = SDIOResetCounter {
-        i8uAddress: dio_address,
-        i16uBitfield: bitfield,
-    };
-    ioctl(fd, KBRequests::DIOResetCounter, &mut ctr).map(|_| ())
+    ctr: *mut SDIOResetCounter,
+) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::DIOResetCounter, ctr).map(|_| ())
 }
 
-pub fn get_last_message<F: AsRawFd>(fd: F) -> Result<CString, PiControlRawError> {
-    let mut msg = Vec::with_capacity(REV_PI_ERROR_MSG_LEN);
-    unsafe {
-        ioctl(fd, KBRequests::GetLastMessage, &mut msg)?;
-        let len = libc::strlen(msg.as_ptr() as *const i8);
-        msg.set_len(len + 1);
-    }
-    // Should never panic, we trust the api
-    Ok(CString::new(msg).unwrap())
+pub unsafe fn get_last_message<F: AsRawFd>(fd: F, msg: *mut i8) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::GetLastMessage, msg).map(|_| ())
 }
 
-pub fn stop_io<F: AsRawFd>(fd: F) -> Result<(), PiControlRawError> {
-    let mut stop = 1i32;
-    unsafe { ioctl(fd, KBRequests::StopIO, &mut stop) }?;
-    Ok(())
+pub unsafe fn stop_io<F: AsRawFd>(fd: F, stop: *mut i32) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::StopIO, stop).map(|_| ())
 }
 
-// TODO add toggle?
-pub fn start_io<F: AsRawFd>(fd: F) -> Result<(), PiControlRawError> {
-    let mut start = 0i32;
-    unsafe { ioctl(fd, KBRequests::StopIO, &mut start) }?;
-    Ok(())
+pub unsafe fn set_output_watchdog<F: AsRawFd>(fd: F, millis: *mut u32) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::SetOutputWatchdog, millis).map(|_| ())
 }
 
-pub fn set_output_watchdog<F: AsRawFd>(fd: F, mut millis: u32) -> Result<(), PiControlRawError> {
-    unsafe { ioctl(fd, KBRequests::SetOutputWatchdog, &mut millis) }.map(|_| ())
-}
-
-pub fn wait_for_event<F: AsRawFd>(fd: F, mut event: Event) -> Result<(), PiControlRawError> {
-    unsafe { ioctl(fd, KBRequests::WaitForEvent, &mut event) }.map(|_| ())
+pub unsafe fn wait_for_event<F: AsRawFd>(fd: F, event: *mut i32) -> PiControlRawResult<()> {
+    ioctl(fd, KBRequests::WaitForEvent, event).map(|_| ())
 }
