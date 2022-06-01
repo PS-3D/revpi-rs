a71ef8db9725de08e2057de3e08667e48123555e
Maximilian Fischer <mfischer@ps-3d.de>
2022-06-01 18:19:51 +0200

    Added PiControl struct

--- src/picontrol.rs
+++ src/picontrol.rs
@@ -1,8 +1,11 @@
 pub mod raw;
 
-use std::{ffi::CString, io};
-use crate::util::ensure;
 use self::raw::{raw::SPIVariable, Bit, PiControlRaw};
+use crate::util::ensure;
+use std::{
+    ffi::{self, CString},
+    io,
+};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -22,6 +25,76 @@ pub enum PiControlError {
     /// Wrapper around [`io::Error`]
     #[error(transparent)]
     IoError(#[from] io::Error),
+    /// Wrapper around [`ffi::NulError`]
+    #[error(transparent)]
+    NulError(#[from] ffi::NulError),
 }
 
+#[derive(Debug)]
+pub struct PiControl {
+    inner: PiControlRaw,
+}
 
+#[derive(Debug, PartialEq, Hash, Clone, Copy)]
+pub enum Value {
+    Bit(bool),
+    Byte(u8),
+    Word(u16),
+    DWord(u32),
+}
+
+impl Value {
+    pub fn bitcnt(&self) -> usize {
+        use Value::*;
+        match self {
+            Bit(_) => 1,
+            Byte(_) => u8::BITS as usize,
+            Word(_) => u16::BITS as usize,
+            DWord(_) => u32::BITS as usize,
+        }
+    }
+}
+
+impl PiControl {
+    pub fn new() -> Result<Self, PiControlError> {
+        Ok(Self {
+            inner: PiControlRaw::new()?,
+        })
+    }
+
+    fn find_variable(&self, name: &str) -> Result<SPIVariable, PiControlError> {
+        self.inner
+            .find_variable(&CString::new(name).map_err(PiControlError::from)?)
+    }
+
+    pub fn set_value(&self, name: &str, value: Value) -> Result<(), PiControlError> {
+        let name = self.find_variable(name)?;
+        ensure!(
+            name.i16uLength as usize == value.bitcnt(),
+            PiControlError::InvalidArgument("value or str")
+        );
+        match value {
+            Value::Bit(b) => unsafe {
+                self.inner
+                    .set_bit(name.i16uAddress, Bit::from(name.i8uBit), b)
+            },
+            Value::Byte(b) => unsafe { self.inner.set_byte(name.i16uAddress, b) },
+            Value::Word(w) => unsafe { self.inner.set_word(name.i16uAddress, w) },
+            Value::DWord(d) => unsafe { self.inner.set_dword(name.i16uAddress, d) },
+        }
+    }
+
+    pub fn get_value(&self, name: &str) -> Result<Value, PiControlError> {
+        let name = self.find_variable(name)?;
+        let res = match name.i16uLength {
+            1 => {
+                Value::Bit(unsafe { self.inner.get_bit(name.i16uAddress, Bit::from(name.i8uBit)) }?)
+            }
+            8 => Value::Byte(unsafe { self.inner.get_byte(name.i16uAddress) }?),
+            16 => Value::Word(unsafe { self.inner.get_word(name.i16uAddress) }?),
+            32 => Value::DWord(unsafe { self.inner.get_dword(name.i16uAddress) }?),
+            _ => panic!("invalid bitlength from piControl"),
+        };
+        Ok(res)
+    }
+}
